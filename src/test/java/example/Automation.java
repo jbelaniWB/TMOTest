467ac5c990a304066a0c5c4227924eca6ae9d9c2
QAMacbookPro <qamacbookpro@QAMacbookPros-MacBook-Pro.local>
2017-05-01 12:57:05 -0700

    Added SauceLabs support

---
+++ src/test/java/example/Automation.java
@@ -0,0 +1,50 @@
+package example;
+
+import org.junit.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import example.TestBase;
+
+public class Automation{
+
+//	public Automation(String os, String version, String browser, String deviceName, String deviceOrientation) {
+//		super(os, version, browser, deviceName, deviceOrientation);
+//		// TODO Auto-generated constructor stub
+//	}
+//
+//	@Test				
+//	public void testSelenium() {	
+//		driver.get("http://demo.guru99.com/selenium/guru99home/");  
+//		String title = driver.getTitle();				 
+//		Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
+//	}
+
+private WebDriver driver;		
+@Test				
+public void testEasy() {	
+	driver.get("http://demo.guru99.com/selenium/guru99home/");  
+	String title = driver.getTitle();				 
+	Assert.assertTrue(title.contains("Demo Guru99 Page")); 	
+	System.out.println("Aumation class");
+}	
+@BeforeTest
+public void beforeTest() {	
+	System.setProperty("webdriver.chrome.driver", "chromedriver");
+    driver = new ChromeDriver();
+//	try {
+//		driver = new RemoteWebDriver(new java.net.URL("http://127.0.0.1:7055"), DesiredCapabilities.chrome());
+//	} catch (MalformedURLException e) {
+//		// TODO Auto-generated catch block
+//		e.printStackTrace();
+//	}
+}		
+@AfterTest
+public void afterTest() {
+	driver.quit();			
+}
+}
--- src/test/java/example/NewTest.java
+++ src/test/java/example/NewTest.java
@@ -1,11 +1,18 @@
 package example;
 
 import org.testng.annotations.Test;
+
+import Pages.GuineaPigPage;
+
 import org.testng.Assert;
 
+import java.lang.reflect.Method;
 import java.net.MalformedURLException;
+import java.rmi.UnexpectedException;
+import java.util.UUID;
 
-import org.openqa.selenium.By;		
+import org.openqa.selenium.By;
+import org.openqa.selenium.InvalidElementStateException;
 import org.openqa.selenium.WebDriver;		
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -14,27 +21,41 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
 
-public class NewTest {
-	private WebDriver driver;		
-	@Test				
-	public void testEasy() {	
-		driver.get("http://demo.guru99.com/selenium/guru99home/");  
-		String title = driver.getTitle();				 
-		Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
-	}	
-	@BeforeTest
-	public void beforeTest() {	
-		System.setProperty("webdriver.chrome.driver", "/Users/qamacbookpro/documents/SupportLibraries/chromedriver");
-	    driver = new ChromeDriver();
-//		try {
-//			driver = new RemoteWebDriver(new java.net.URL("http://127.0.0.1:7055"), DesiredCapabilities.chrome());
-//		} catch (MalformedURLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-	}		
-	@AfterTest
-	public void afterTest() {
-		driver.quit();			
-	}
+public class NewTest extends TestBase{
+//	private WebDriver driver;		
+//	@Test				
+//	public void testEasy() {	
+//		driver.get("http://demo.guru99.com/selenium/guru99home/");  
+//		String title = driver.getTitle();				 
+//		Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
+//	}	
+//	@BeforeTest
+//	public void beforeTest() {	
+//		System.setProperty("webdriver.chrome.driver", "chromedriver");
+//	    driver = new ChromeDriver();
+////		try {
+////			driver = new RemoteWebDriver(new java.net.URL("http://127.0.0.1:7055"), DesiredCapabilities.chrome());
+////		} catch (MalformedURLException e) {
+////			// TODO Auto-generated catch block
+////			e.printStackTrace();
+////		}
+//	}		
+//	@AfterTest
+//	public void afterTest() {
+//		driver.quit();			
+//	}
+	@org.testng.annotations.Test(dataProvider = "hardCodedBrowsers")
+    public void verifyCommentInputTest(String browser, String version, String os, Method method)
+            throws MalformedURLException, InvalidElementStateException, UnexpectedException {
+        this.createDriver(browser, version, os, method.getName());
+        WebDriver driver = this.getWebDriver();
+
+        String commentInputText = UUID.randomUUID().toString();
+
+        GuineaPigPage page = GuineaPigPage.visitPage(driver);
+
+        page.submitComment(commentInputText);
+
+        Assert.assertTrue(page.getSubmittedCommentText().contains(commentInputText));
+    }
 }
