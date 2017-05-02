8f845641aa519b68487cfd1b9e9f248162d0d5a5
QAMacbookPro <qamacbookpro@QAMacbookPros-MacBook-Pro.local>
2017-05-02 12:48:05 -0700

    Testing adding a new class to project

---
+++ src/test/java/example/NewTestClass.java
@@ -0,0 +1,29 @@
+package example;
+
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.rmi.UnexpectedException;
+import java.util.UUID;
+
+import org.openqa.selenium.InvalidElementStateException;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+import Pages.GuineaPigPage;
+
+public class NewTestClass extends TestBase{
+	@org.testng.annotations.Test(dataProvider = "hardCodedBrowsers")
+    public void verifyCommentInputTest_NewTest(String browser, String version, String os, Method method)
+            throws MalformedURLException, InvalidElementStateException, UnexpectedException {
+        this.createDriver(browser, version, os, method.getName());
+        WebDriver driver = this.getWebDriver();
+
+        String commentInputText = UUID.randomUUID().toString();
+
+        GuineaPigPage page = GuineaPigPage.visitPage(driver);
+
+        page.submitComment(commentInputText);
+
+        Assert.assertTrue(page.getSubmittedCommentText().contains(commentInputText));
+    }
+}
